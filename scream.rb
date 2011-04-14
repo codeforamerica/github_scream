bdf965a8a7e209f6dfcb44bd2e20f4b9d28ebb8b
T S <YenTheFirst@gmail.com>
2011-04-13 23:04:16 -0700

    make hash pronouncable

--- scream.rb
+++ scream.rb
@@ -24,7 +24,7 @@ post "/receive_commit" do
 		next if commit["message"].match /^Merge branch/		
 		message = commit["author"]["name"]+" pushed to "+repo["name"]+", "+commit["message"]
 		message.gsub!(/["']/,'')
-		message.gsub! '#',' # '
+		message.gsub! '#',' hash '
 		puts "playing '#{message}'"
 		system("echo '#{message}' | festival --tts")
 		#sleep 5
