8dbcd332e6d68259b76cffc2df194747eb4c5957
T S <YenTheFirst@gmail.com>
2011-04-14 11:39:42 -0700

    made sound_repo configurable, added sound grouping for bulk pushes.

--- scream.rb
+++ scream.rb
@@ -1,33 +1,43 @@
 require 'pp'
 require 'yaml'
-$sounds=YAML::load_file("cfa_coder_sounds/list.yml")
+
+$config = YAML::load_file("config.yml")
+$sound_repo = $config["sound_repo"]
+Dir.chdir($sound_repo) #treat our sounds repo as our current working directory, so we can directly access the config list and the sounds, without prefixing.
+$sounds = YAML::load_file("list.yml")
+
 
 post "/receive_commit" do
 	push = JSON.parse(params[:payload])
 	puts "got push: "
 	pp push
 
-	#for now, assume all commits are by the same coder.
-	author = push["commits"][0]["author"]
 	repo = push["repository"]
 
-	if repo["name"]=="cfa_coder_sounds"
-		system("git submodule foreach git pull origin master")
-		$sounds=YAML::load_file("cfa_coder_sounds/list.yml")
+	if repo["name"] == $sound_repo
+		system("git pull origin master")
+		$sounds = YAML::load_file("list.yml")
 	end
-
 	
-	sound = $sounds["users"].values_at(*author.values).find {|x| !x.nil?} || ($sounds["projects"][repo["owner"]["name"]] || {})[repo["name"]] || $sounds["default"]
-	system("mpg321 cfa_coder_sounds/sounds/#{sound}")
+	#group commits by the announcing sound
+	commits_by_sound = push["commits"].group_by do |commit|
+		sound = $sounds["users"].values_at(*commit["author"].values).find {|x| !x.nil?}	#announcing sound is the configured user-specific sound
+		sound ||= ($sounds["projects"][repo["owner"]["name"]] || {})[repo["name"]]		#if that didn't work, try the project-specific sound
+		sound ||= $sounds["default"]													#otherwise, the default sound
+		sound
+	end
 	
-	push["commits"].each do |commit|
-		next if commit["message"].match /^Merge branch/		
-		message = commit["author"]["name"]+" pushed to "+repo["name"]+", "+commit["message"]
-		message.gsub!(/["']/,'')
-		message.gsub! '#',' hash '
-		puts "playing '#{message}'"
-		system("echo '#{message}' | festival --tts")
-		#sleep 5
+	commits_by_sound.each do |sound,commits|
+		system("mpg321 sounds/#{sound}")
+		commits.each do |commit|
+			next if commit["message"].match /^Merge branch/
+			
+			message = commit["author"]["name"]+" pushed to "+repo["name"]+", "+commit["message"]
+			message.gsub!(/["']/,'')
+			message.gsub! '#',' hash '
+			puts "playing '#{message}'"
+			system("echo '#{message}' | festival --tts")
+		end
 	end
 	
 	[200,{},"coo"]
